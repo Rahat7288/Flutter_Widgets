27ffbfbfc1e24015bd4932653cbd0f7fcc750890
Rahat <rahatislamakash@gmail.com>
2022-05-05 23:16:23 +0600

    updating flutter installation file

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Screen(),
+    )
+  );
+}
+
+class Screen extends StatefulWidget {
+  const Screen({Key? key}) : super(key: key);
+
+  @override
+  State<Screen> createState() => _ScreenState();
+}
+
+class _ScreenState extends State<Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //backgroundColor: Colors.black26,
+
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 40.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text('data'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
